32872b61c806f016ce089d0e2c341ff617e2f62a
Cosmmoss <pavelandreevich16@gmail.com>
2024-06-06 23:34:39 +0300

    Fixed 2,3,4 tasks from for_dict_challenges_bonus

--- for_dict_challenges_bonus.py
+++ for_dict_challenges_bonus.py
@@ -69,17 +69,17 @@ def generate_chat_history():
 def user_wrote_most_messages(history):
     
     user_id_message = {}  # создаётся словарь с парой - 'user_id': кол-во сообщений
-    for user_id in history:
-        user_id_message[user_id['sent_by']] = user_id_message.get(user_id['sent_by'], 0) + 1
+    for message in history:
+        user_id_message[message['sent_by']] = user_id_message.get(message['sent_by'], 0) + 1
 
     user_id_max = max(user_id_message.items(), key=lambda items: items[1])  # определяю максимальное количество сообщений
     
-    users_id_max = [] 
-    for key, value in user_id_message.items():  # для определения всех айди пользователей, которые написали больше всех сообщений
-        if value >= user_id_max[1]:
-            users_id_max.append(key)
+    ids_with_max_messages = [] 
+    for user_id, message_count in user_id_message.items():  # для определения всех айди пользователей, которые написали больше всех сообщений
+        if message_count == user_id_max[1]:
+            ids_with_max_messages.append(user_id)
 
-    return f'ID пользователя(ей), который(е) написал(и) больше всех сообщений: {users_id_max}'
+    return f'ID пользователя(ей), который(е) написал(и) больше всех сообщений: {ids_with_max_messages}'
 
 # 2. Вывести айди пользователя, на сообщения которого больше всего отвечали.
 def user_messages_most_answered(history):
@@ -87,66 +87,60 @@ def user_messages_most_answered(history):
     message_db = {}
     for message in history:  # создаём словарь ключ (id message): значение (весь message)
         message_db[message['id']] = message
+    
     users_counter = {}
-    for message in message_db.values():  # перебираем значения, созданного выше словаря message_db
-        if message['reply_for'] is not None:  # если ответ на сообщение пользователя не None
-            parent_message = message  # в переменную parent_message передаётся весь message
-            user_id = parent_message['sent_by']  # в переменную user_id передаётся ID пользователя
-            users_counter[user_id] = users_counter.get(user_id, 0) + 1  # создаётся словарь ключ (ID пользователя): значение (количество сообщений, направленных ему)
+    for message in history:  # перебираем сообщения в списке history
+        if message['reply_for'] is not None:  # если id (сообщение, на которое есть ответ) не None
+            parent_message = message_db[message['reply_for']]  # в правой части передаём id сообщ. message['reply_for'] по ключу id сообщ. словаря message_db. Таким образом в переменную передаётся словарь, в котором по ключу 'sent_by' id пользователя, получившего ответ на id сообщение из message['reply_for']
+            user_id = parent_message['sent_by']  # в переменную user_id передаётся ID пользователя, получившего ответ
+            users_counter[user_id] = users_counter.get(user_id, 0) + 1  # создаётся словарь ключ (ID пользователя): значение (количество сообщений)
     
     user_id_max = max(users_counter.items(), key=lambda items: items[1])  # определяется кортеж (ID пользователя, максимальное количество сообщений)
     
     users_id_max = [] 
-    for key, value in users_counter.items():  # для определения всех айди пользователей (в случае повторений максимального количества сообщений), на сообщения которого больше всего отвечали
+    for key, value in users_counter.items():  # для определения всех айди пользователей (в случае повторений максимального количества сообщений)
         if value == user_id_max[1]:
             users_id_max.append(key)
 
-    return f'ID пользователя(ей), на сообщения которого(ых) больше всего отвечали: {users_id_max}'
+    return f'ID пользователя(ей), на сообщения которого(ых) больше всего отвечали: {users_id_max}' 
 
 # 3. Вывести айди пользователей, сообщения которых видело больше всего уникальных пользователей.
 def id_users_posts_seen_most_uniq_users(history):
-    messages_db = {}
+    
+    id_user_uniq_users = {}  # создаём словарь - ключ (id_message): значение (количество уникальных пользователей)
     for message in history:
-        messages_db[message['id']] = message
+        id_user_uniq_users[message['id']] = len(message['seen_by']) # по ключу id_message передаем количество уникальных пользователей
     
-    id_user_uniq_users = {}  # создаём словарь ключ (id_messege): значение (количество уникальных пользователей)
-    for mess in messages_db.values():
-        id_user_uniq_users[mess['id']] = mess['seen_by'] = len(mess['seen_by']) # замена значения по ключу seen_by в словаре messages_db
-    
-    num_max_uniq_user = max(id_user_uniq_users.items(), key=lambda items: items[1])  # определяется кортеж (id_messege, максимальное количество уникальных пользователей)
+    num_max_uniq_users = max(id_user_uniq_users.items(), key=lambda items: items[1])  # определяется кортеж (id_messege, максимальное количество уникальных пользователей)
+  
+    users_id_max = []  # создаём список, чтобы элементы были типа int; будут повторяться одинаковые id_user 
+    for message in history:
+        if len(message['seen_by']) == num_max_uniq_users[1]:  # если кол-во уник. сообщ. равно максимальному кол-ву
+            users_id_max.append(message['sent_by'])  # в список добавить ID пользователя
 
-    # users_id_max = {}  можно создать словарь и подсчитать максимальное количество уникальных просмотров с учётом повторяющихся id_user
-    users_id_max = set()  # создаём множество, чтобы не дублировались одинаковые id_user
-    for message in messages_db.values():
-        if message['seen_by'] == num_max_uniq_user[1]:
-            # users_id_max[message['sent_by']] = users_id_max.get(message['sent_by'], 0) + 1
-            users_id_max.add(message['sent_by'])
-      
     return f'ID пользователей, сообщения которых видело больше всего уникальных пользователей: {users_id_max}'
 
 # 4. Определить, когда в чате больше всего сообщений: утром (до 12 часов), днём (12-18 часов) или вечером (после 18 часов).
 
 def time_most_messages_in_chat(history):
-    # Период времени с 00:00:00 по 05:59:59 не учитывается в соответствии с заданием
 
     num_all_messages = {                  # словарь - ключ (период времени): значение (id сообщений за это время). 
             'утром (до 12 часов)': [],
             'днём (12-18 часов)': [],
             'вечером (после 18 часов)': []
             }
-
+  
     for message in history:
-        if '06:00:00' <= message['sent_at'].strftime('%H:%M:%S') <= '12:00:00':  # если время сообщения больше заданного
-            num_all_messages['утром (до 12 часов)'].append(message['id'])  # добавить в список словаря num_all_message
-        elif '12:00:01' <= message['sent_at'].strftime('%H:%M:%S') <= '18:00:00':
+        if 0 <= message['sent_at'].hour < 12:  # если время сообщения больше заданного
+            num_all_messages['утром (до 12 часов)'].append(message['id'])  # добавить 'id' message в список словаря num_all_message
+        elif 12 <= message['sent_at'].hour < 18:
             num_all_messages['днём (12-18 часов)'].append(message['id'])
-        elif '18:00:01' <= message['sent_at'].strftime('%H:%M:%S') <= '23:59:59':
+        elif 18 <= message['sent_at'].hour < 24:
             num_all_messages['вечером (после 18 часов)'].append(message['id'])
-
+    
     time_max_messages = (max(num_all_messages.items(), key=lambda items: len(items[1])))  # определяем в какой период времени больше всего сообщений
 
     return f"В чате больше всего сообщений: {time_max_messages[0]}"
-
 
 if __name__ == "__main__":
     history = generate_chat_history()
